59ff16eb5d8a5ea74335ed2e415cd6964e64d2e4
manosanjay-g <manosanjay.web@gmail.com>
2022-04-20 19:52:50 +0530

    initial_commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+
+const app = express();
+const connectDB = require('./configs/database_config')
+require('dotenv').config();
+
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is listening on PORT ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("Database connection failed. Exiting now...");
+        console.log(error);
+        process.exit(1);
+    }
+}
+start()
